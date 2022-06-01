b2ed450a1261e8180c0d81b59e5011d6e5952c0b
aguAratech <agustin.carmona@ara-tech.es>
2022-06-01 14:25:11 -0700

    logic for tags control

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import Body from './Body';
+import React, { useEffect, useState } from 'react'
+import BuyFonts from './BuyFonts'
+import MyFonts from './MyFonts'
 
-import NavBar from './NavBar';
 
 function Main() {
+
+const stringMyFonts = 'my fonts'
+const stringBuyFonts = 'buy fonts'
+
+const [tagToShow, setTagToShow] = useState(stringMyFonts)
+
+let componentToShow =  <BuyFonts/>
+
+function handlerTags(tagsName) {
+  setTagToShow(tagsName)
+}
+
+
+useEffect( ()=> {
+  switch (tagToShow) {
+    case stringMyFonts:
+      componentToShow = <MyFonts/>
+      break;
+
+    case stringBuyFonts:
+      componentToShow = <BuyFonts/>
+      break;
+  
+    default:
+      break;
+  }
+}, [tagToShow] )
+
+
+
+
+
   return (
-    <div className='main'>
-      <NavBar />
-      <Body />
-    </div>
+    <>
+      <div className='main'>
+        <div className='main__navbar'>
+          <p className='main__navbar__sentence-title'>Please, select one font</p>
+          <div className='main__navbar__tags-container'>
+            <p onClick={()=>handlerTags(stringMyFonts)}>MY FONTS</p>
+            <p onClick={()=>handlerTags(stringBuyFonts)}>BUY FONTS</p>
+        </div>
+        </div>
+        <div className='main__body'>
+        {/* { tagToShow === stringMyFonts ? <MyFonts />: undefined } */}
+        { tagToShow === stringBuyFonts ? <BuyFonts /> : <MyFonts /> }
+        </div>
+      </div>
+    </>
   )
 }
 

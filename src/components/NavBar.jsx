378e8c0261f4c48ed7a3ac4a215c48c54860901d
aguAratech <agustin.carmona@ara-tech.es>
2022-06-01 12:37:02 -0700

    navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 
+function handlerTags(tagsName) {
+  console.log('PACO', tagsName)
+}
+
+
 function NavBar() {
   return (
     <div className='navbar'>
       <p className='navbar__sentence-title'>Please, select one font</p>
       <div className='navbar__tags-container'>
-        <p>MY FONTS</p>
-        <p>BUY FONTS</p>
+        <p onClick={()=>handlerTags('my fonts')}>MY FONTS</p>
+        <p onClick={()=>handlerTags('buy fonts')}>BUY FONTS</p>
       </div>
     </div>
   )
